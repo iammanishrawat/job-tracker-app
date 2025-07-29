026781099755fce600cc50178d117836e394e598
Manish rawat <manish.rawat@essencesoftwares.com>
2025-07-29 23:09:25 +0530

    maping data added

---
+++ src/data/jobs.js
@@ -0,0 +1,18 @@
+const jobs = [
+    {
+        id: 1,
+        title: "Frontend Developer",
+        company: "TechCorp",
+        location: "Remote",
+        status: "Interview",
+    },
+    {
+        id: 2,
+        title: "Backend Engineer",
+        company: "NodeHouse",
+        location: "Bangalore",
+        status: "Pending",
+    },
+];
+
+export default jobs;
--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -1,26 +1,30 @@
+import jobs from '../data/jobs'
+
 const AllJobs = () => {
   return (
     <div className="p-6 max-w-3xl mx-auto">
       <h2 className="text-2xl font-bold mb-6">All Jobs</h2>
 
       <div className="space-y-4">
-        <div className="border p-4 rounded shadow">
-          <h3 className="text-lg font-semibold">Frontend Developer</h3>
-          <p>Company: Google</p>
-          <p>Location: Remote</p>
-          <p>
-            Status: <span className="text-yellow-600">Pending</span>
-          </p>
-        </div>
+        {jobs.map((job) => (
+          <div className="border p-4 rounded shadow">
+            <h3 className="text-lg font-semibold">{job.title}</h3>
+            <p>Company=: {job.company}</p>
+            <p>Location: {job.location}</p>
+            <p>
+              Status: <span className="text-yellow-600">{job.status}</span>
+            </p>
+          </div>
+        ))}
 
-        <div className="border p-4 rounded shadow">
+        {/* <div className="border p-4 rounded shadow">
           <h3 className="text-lg font-semibold">React Developer</h3>
           <p>Company: Infosys</p>
           <p>Location: Bangalore</p>
           <p>
             Status: <span className="text-green-600">Interview</span>
           </p>
-        </div>
+        </div> */}
       </div>
     </div>
   )
