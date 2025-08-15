6e9c450671a3a09dcfcbc5e67f896586806b4352
Manish rawat <manish.rawat@essencesoftwares.com>
2025-08-15 18:18:38 +0530

    list added with api url

--- src/pages/jobsList.jsx
+++ src/pages/jobsList.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
-import allJobsData from '../data/allJobsData'
+import { useEffect, useState } from 'react'
 
 function JobList() {
-  const [jobsData, setJobsData] = useState(allJobsData)
+  const [jobData, setJobData] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonfakery.com/jobs')
+      .then((response) => response.json())
+      .then((data) => {
+        setJobData(data)
+      })
+      .catch((error) => {
+        alert('list is not available', error)
+      })
+  }, [])
   return (
     <>
       {/* yha se job listing map hogi or jitni data mein jobs hogi utni dikhegi */}
       <ul>
-        {jobsData.map((jobs) => {
+        {jobData.map((jobs) => {
           return (
             <li key={jobs.id}>
-              Title: <span>{jobs.title}</span>, Company:{' '}
-              <span>{jobs.company}</span>
+              Title: <span>{jobs.title}</span>, Company:
+              <span>{jobs.company}</span>, Location:<span>{jobs.location}</span>
+              , Contact: <span>{jobs.contact}</span>,Remote Job:
+              <span>{jobs.is_remote_work}</span>
             </li>
           )
         })}
