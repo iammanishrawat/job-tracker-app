f1d88ab391a54da5d3a0b9e81680c2ab44ba1ced
Manish rawat <manish.rawat@essencesoftwares.com>
2025-08-24 23:00:04 +0530

    now adding product list here

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import Jobs from './components/Jobs'
-
 function App() {
   return (
     <>
-      <h1 className="mb-5">Job Tracker App</h1>
-      <Jobs />
+      <h1 className="mb-5">Products</h1>
     </>
   )
 }
