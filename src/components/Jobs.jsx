f3bcdc14af26a8d361395276f9f71bc72b9c6390
Manish rawat <manish.rawat@essencesoftwares.com>
2025-09-10 00:39:27 +0530

    list added again

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,23 +1,8 @@
-import AddJob from '../pages/AddJob'
 import JobsList from '../pages/JobsList'
-import { useEffect, useState } from 'react'
-const Jobs = (jobData) => {
-  const [jobListData, setJobListData] = useState([jobData])
-  useEffect(() => {
-    fetch('https://jsonfakery.com/jobs')
-      .then((res) => res.json())
-      .then((data) => {
-        setJobListData(data)
-      })
-      .catch(() => {
-        alert('Data Not found')
-      })
-  }, [])
+const Jobs = () => {
   return (
     <>
-      <h1 className="mb-5">Add New Jobs</h1>
-      <AddJob addJob={addJob} />
-      <JobsList jobData={jobListData} />
+      <JobsList />
     </>
   )
 }
--- src/pages/jobsList.jsx
+++ src/pages/jobsList.jsx
@@ -1,14 +1,31 @@
-const JobsList = ({ jobData }) => {
+import { useEffect, useState } from 'react'
+
+const JobsList = () => {
+  const [jobListData, setJobListData] = useState([])
+  const fetchJobList = () => {
+    fetch('http://localhost:3000/jobs')
+      .then((res) => res.json())
+      .then((data) => {
+        setJobListData(data)
+      })
+      .catch(() => {
+        alert(`no data found`)
+      })
+  }
+  useEffect(() => {
+    fetchJobList()
+  }, [])
   return (
     <>
+      <h1>Jobs</h1>
       <div className="cards-grid">
-        {jobData.map((jobs) => {
+        {jobListData.map((data) => {
           return (
-            <div className="card" key={jobs.id}>
-              <h4>Job Title: {jobs.title}</h4>
-              <p>Job Description: {jobs.description}</p>
-              <p>Job Location: {jobs.location}</p>
-              <p>Company: {jobs.company}</p>
+            <div className="card">
+              <h4>Title: {data.title}</h4>
+              <p>Company: {data.company}</p>
+              <p>Location: {data.location}</p>
+              <p>Salary: {data.salary}</p>
             </div>
           )
         })}
